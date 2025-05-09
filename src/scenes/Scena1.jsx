cc62f5ef02895dccc126d394aceb6708136cb85c
Jonnycp <jonathan-caputo@hotmail.com>
2025-05-09 08:50:37 +0200

    Aggiunta animazione unroll

--- src/scenes/Scena1.jsx
+++ src/scenes/Scena1.jsx
@@ -2,32 +2,27 @@ import { useState } from "react";
 import ImageMapper from "react-img-mapper";
 import casa from "@assets/images/Casa.png";
 import pergamena from "@assets/images/Pergamena.png";
+import Button from "@components/Button";
 
 const Scena1 = () => {
   const [load, setLoad] = useState([false, false]);
   const [cliccable, setCliccable] = useState(false);
+  const [scene, setScene] = useState(1);
 
   return (
-    <>
-      <ImageMapper
-        src={casa}
-        name="Casa di Carla"
-        natural
-        imgWidth={1920}
-        parentWidth={window.innerWidth > 1920 ? 1920 : window.innerWidth}
-        responsive={true}
-        disabled={!cliccable}
-        areas={null}
-        //imgProps={}
-        //canvasProps={}
-        onChange={() => {}}
-        isMulti={false}
-        onLoad={() => setLoad([true, true])}
-      />
-      {!load[1] && (
-        <img src={casa} alt="Casa di Carla" className={"select-none "} />
-      )}
-    </>
+    <section className="w-full h-svh flex flex-col items-center justify-center relative">
+      {scene == 1 && 
+      <img
+        src={pergamena}
+        alt="Pergamena"
+        onLoad={() => setLoad(prev => [true, prev[1]])}
+        className={"absolute z-10 animate-unroll select-none h-full"}
+      />}
+
+      <img src={casa} alt="Casa di Carla" className={`absolute h-full select-none ${load[0] && scene <= 1 ? "opacity-10" : ""}`} />
+      
+      <Button classes="absolute bottom-2 right-4" onClick={()=>{setScene(2)}}>Avanti</Button>
+    </section>
   );
 };
 
