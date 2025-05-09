9de0d94f24db7ba868ad421c338809d48cf32be4
Jonnycp <jonathan-caputo@hotmail.com>
2025-05-09 12:23:38 +0200

    Istallability rompe

--- src/Adelfia.jsx
+++ src/Adelfia.jsx
@@ -8,10 +8,13 @@ import SplashScreen from "./scenes/SplashScreen";
 
 import Scena1 from "./scenes/Scena1";
 import Scena2 from "./scenes/Scena2";
+import Scena3 from "./scenes/Scena3";
 import Timer from "@components/Timer";
 
 const Bitritto = () => {
-  const [isPortrait, setIsPortrait] = useState(window.matchMedia("(orientation: portrait)").matches);
+  const [isPortrait, setIsPortrait] = useState(
+    window.matchMedia("(orientation: portrait)").matches
+  );
   const [installation, setInstallation] = useState(false);
 
   useEffect(() => {
@@ -28,60 +31,81 @@ const Bitritto = () => {
 
   // Installa PWA
   useEffect(() => {
-    if (installation) return; // Se l'app è già installata, non fare nulla
-    let deferredPrompt;
+    if (!installation) {
+      let deferredPrompt;
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      e.preventDefault();
-      deferredPrompt = e;
+      window.addEventListener("beforeinstallprompt", (e) => {
+        e.preventDefault();
+        deferredPrompt = e;
 
-      // Attendere un breve tempo prima di mostrare il prompt
-      setTimeout(() => {
-        if (deferredPrompt) {
-          deferredPrompt.prompt();
-          deferredPrompt.userChoice
-            .then((choiceResult) => {
+        // Attendere un breve tempo prima di mostrare il prompt
+        setTimeout(() => {
+          setInstallation(true);
+          if (deferredPrompt) {
+            deferredPrompt.prompt();
+            deferredPrompt.userChoice.then((choiceResult) => {
               if (choiceResult.outcome === "accepted") {
                 console.log("Utente ha accettato l'installazione");
               } else {
                 console.log("Utente ha rifiutato l'installazione");
               }
               deferredPrompt = null;
-              setInstallation(true);
             });
-        }
-      }, 1000);
-    });
+          }
+        }, 1000);
+      });
+    }
 
     return () => {
-      window.removeEventListener('beforeinstallprompt', () => {});
+      window.removeEventListener("beforeinstallprompt", () => {});
     };
   }, [installation]);
 
-
   const [startTime, setStartTime] = useState(false);
- 
+
+  useEffect(() => {
+    if(window.location.hash == '#/scena2' || window.location.hash == '#/scena3') {
+      setStartTime(true);
+    }else if(window.location.hash == '#/gameover' || window.location.hash == '#/win') {
+      setStartTime(false);
+    }else if(window.location.hash == '#/') {
+      setStartTime(false);
+    }
+  }, [window.location.hash]);
   return (
     <>
       {isPortrait ? (
         <RotatePhone />
       ) : (
         <HashRouter>
-          {startTime && <Timer startTime={startTime} setStartTime={setStartTime}/>}
+          {startTime && (
+            <Timer startTime={startTime} setStartTime={setStartTime} />
+          )}
           <Routes>
-            <Route path="/" element={<SplashScreen location={'Adelfia'} title={'La faida tra Montrone e Canneto'} />} />
-            <Route path="/scena1" element={<Scena1 setTimer={setStartTime}/>} />
-            <Route path="/scena2" element={<Scena2/>} />
-            {/* <Route path="/scena3" element={<Scena3/>} />
-            <Route path="/scena4" element={<Scena4/>} />
+            <Route
+              path="/"
+              element={
+                <SplashScreen
+                  location={"Adelfia"}
+                  title={"La faida tra Montrone e Canneto"}
+                />
+              }
+            />
+            <Route
+              path="/scena1"
+              element={<Scena1 setTimer={setStartTime} />}
+            />
+            <Route path="/scena2" element={<Scena2 />} />
+            <Route path="/scena3" element={<Scena3 />} />
+            {/*<Route path="/scena4" element={<Scena4/>} />
             <Route path="/scena5" element={<Scena5/>} />
             <Route path="/scena6" element={<Scena6/>} />
             <Route path="/scena7" element={<Scena7/>} />
             <Route path="/engine" element={<JSONEngine/>} /> */}
             {/* Game over */}
-            <Route path="*" element={<GameOver/>} />
+            <Route path="*" element={<GameOver />} />
             {/* Win */}
-            <Route path="/win" element={<Win/>} />
+            <Route path="/win" element={<Win />} />
           </Routes>
         </HashRouter>
       )}
